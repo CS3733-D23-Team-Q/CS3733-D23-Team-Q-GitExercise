d37c658db0c4c8ff6505dd0c4d62d18e8486c3cd
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-03-23 09:18:20 -0400

    fixed println issues

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -5,9 +5,9 @@ public class Main {
   public static void main(String[] args) {
     App.launch(App.class, args);
     System.out.println("Kelu Liu");
-    System.out.print("Cameron Robbins");
-    System.out.print("Josh");
-    System.out.print("Yuran Xue");
+    System.out.println("Cameron Robbins");
+    System.out.println("Josh");
+    System.out.println("Yuran Xue");
   }
 
   // shortcut: psvm
