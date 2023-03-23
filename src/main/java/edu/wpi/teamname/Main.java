2c4322a936b8f62c88873bae6341b8d596de1235
yuranxue272181 <xue272181@gmail.com>
2023-03-22 20:26:16 -0400

    change

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
   public static void main(String[] args) {
     App.launch(App.class, args);
+    System.out.println("Kelu Liu");
+    System.out.print("Cameron Robbins");
+    System.out.print("Josh");
+    System.out.print("Yuran");
   }
 
   // shortcut: psvm
